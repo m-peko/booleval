31235d1b5e526ab9929fd2e5d2fcdd48b17e93d9
m-peko <mpeko00@fesb.hr>
2020-03-10 11:12:55 +0100

    Refactor string utility functions and delete RegexPattern class
    String utility functions are now using C++17 feature std::string_view.
    RegexPattern class is being removed because it turned out that it is
    an overkill to use RegEx for splitting the expressions. Current approach
    has far better performance in providing the same functionality.

--- include/booleval/utils/string_utils.h
+++ include/booleval/utils/string_utils.h
@@ -30,79 +30,181 @@
 #ifndef BOOLEVAL_STRING_UTILS_H
 #define BOOLEVAL_STRING_UTILS_H
 
-#include <string>
 #include <vector>
+#include <string_view>
 
 namespace booleval {
 
 namespace utils {
 
 /**
- * Removes whitespace characters from the left side of the string.
+ * enum class split_options
  *
- * @param str String to remove whitespace characters from
+ * Represents an option used when splitting a string.
  */
-void ltrim(std::string& str);
+enum class split_options : uint8_t {
+    off                = 0x00,
+    include_whitespace = 0x01,
+    include_delimiters = 0x02,
+    exclude_delimiters = 0x04
+};
+
+template <typename Enum>
+constexpr std::enable_if_t<std::is_enum<Enum>::value, Enum>
+operator|(Enum lhs, Enum rhs) {
+    using underlying_type = typename std::underlying_type<Enum>::type;
+    return static_cast<Enum>(
+        static_cast<underlying_type>(lhs) |
+        static_cast<underlying_type>(rhs)
+    );
+}
+
+template <typename Enum>
+constexpr std::enable_if_t<std::is_enum<Enum>::value, Enum>
+operator&(Enum lhs, Enum rhs) {
+    using underlying_type = typename std::underlying_type<Enum>::type;
+    return static_cast<Enum>(
+        static_cast<underlying_type>(lhs) &
+        static_cast<underlying_type>(rhs)
+    );
+}
+
+template <typename Enum>
+constexpr std::enable_if_t<std::is_enum<Enum>::value, Enum>
+operator^(Enum lhs, Enum rhs) {
+    using underlying_type = typename std::underlying_type<Enum>::type;
+    return static_cast<Enum>(
+        static_cast<underlying_type>(lhs) ^
+        static_cast<underlying_type>(rhs)
+    );
+}
+
+template <typename Enum>
+constexpr std::enable_if_t<std::is_enum<Enum>::value, Enum>
+operator~(Enum rhs) {
+    using underlying_type = typename std::underlying_type<Enum>::type;
+    return static_cast<Enum>(
+        ~static_cast<underlying_type>(rhs)
+    );
+}
+
+template <typename Enum>
+constexpr std::enable_if_t<std::is_enum<Enum>::value, Enum>&
+operator|=(Enum& lhs, Enum rhs) {
+    using underlying_type = typename std::underlying_type<Enum>::type;
+    return static_cast<Enum>(
+        static_cast<underlying_type>(lhs) |
+        static_cast<underlying_type>(rhs)
+    );
+}
+
+template <typename Enum>
+constexpr std::enable_if_t<std::is_enum<Enum>::value, Enum>&
+operator&=(Enum& lhs, Enum rhs) {
+    using underlying_type = typename std::underlying_type<Enum>::type;
+    return static_cast<Enum>(
+        static_cast<underlying_type>(lhs) &
+        static_cast<underlying_type>(rhs)
+    );
+}
+
+template <typename Enum>
+constexpr std::enable_if_t<std::is_enum<Enum>::value, Enum>&
+operator^=(Enum& lhs, Enum rhs) {
+    using underlying_type = typename std::underlying_type<Enum>::type;
+    return static_cast<Enum>(
+        static_cast<underlying_type>(lhs) ^
+        static_cast<underlying_type>(rhs)
+    );
+}
 
 /**
- * Removes whitespace characters from the right side of the string.
+ * Checks whether bits of the first set are present in
+ * another the second set and vice versa.
  *
- * @param str String to remove whitespace characters from
+ * @param lhs The first set of bits to check
+ * @param rhs The second set of bits to check
+ *
+ * @return True if the bits are present, false otherwise
  */
-void rtrim(std::string& str);
+template <typename Enum,
+          typename = std::enable_if_t<std::is_enum<Enum>::value>>
+constexpr bool is_set(Enum lhs, Enum rhs) {
+    return (lhs & rhs) == lhs ||
+           (lhs & rhs) == rhs;
+}
 
 /**
- * Removes whitespace characters from the both sides of the string.
+ * Removes whitespace characters from the left side of the string view.
  *
- * @param str String to remove whitespace characters from
+ * @param strv String view to remove whitespace characters from
  */
-void trim(std::string& str);
+void ltrim(std::string_view& strv);
 
 /**
- * Removes whitespace characters from the left side of the string.
+ * Removes whitespace characters from the right side of the string view.
  *
- * @param str String to remove whitespace characters from
- *
- * @return Copy of the modified string
+ * @param strv String view to remove whitespace characters from
  */
-std::string ltrim_copy(std::string str);
+void rtrim(std::string_view& strv);
 
 /**
- * Removes whitespace characters from the right side of the string.
+ * Removes whitespace characters from the both sides of the string view.
  *
- * @param str String to remove whitespace characters from
- *
- * @return Copy of the modified string
+ * @param strv String view to remove whitespace characters from
  */
-std::string rtrim_copy(std::string str);
+void trim(std::string_view& strv);
 
 /**
- * Removes whitespace characters from the both sides of the string.
+ * Removes whitespace characters from the left side of the string view.
  *
- * @param str String to remove whitespace characters from
+ * @param strv String view to remove whitespace characters from
  *
- * @return Copy of the modified string
+ * @return Modified string view
  */
-std::string trim_copy(std::string str);
+std::string_view ltrim_copy(std::string_view strv);
 
 /**
- * Checks whether string is empty or contains only whitespace characters.
+ * Removes whitespace characters from the right side of the string view.
  *
- * @param str String to check
+ * @param strv String view to remove whitespace characters from
  *
- * @return True if string is empty, otherwise false
+ * @return Modified string view
  */
-bool is_empty(std::string const& str);
+std::string_view rtrim_copy(std::string_view strv);
 
 /**
- * Splits string by regular expression.
+ * Removes whitespace characters from the both sides of the string view.
  *
- * @param str     String to split
- * @param pattern Delimiting regular expression pattern
+ * @param strv String view to remove whitespace characters from
  *
- * @return Vector of strings computed by splitting the given string
+ * @return Modified string view
  */
-std::vector<std::string> split(std::string const& str, std::string const& pattern);
+std::string_view trim_copy(std::string_view strv);
+
+/**
+ * Checks whether string view is empty or contains only whitespace characters.
+ *
+ * @param strv String view to check
+ *
+ * @return True if string view is empty, otherwise false
+ */
+bool is_empty(std::string_view strv);
+
+/**
+ * Splits string view by delimiters.
+ *
+ * @param strv    String view to split
+ * @param delims  Delimiters to split the string view by
+ * @param options Options used while splitting the string view
+ *                (by default, whitespace delimiter is included and
+ *                delimiters are excluded from the result)
+ *
+ * @return Tokens computed by splitting the given string view
+ */
+std::vector<std::string_view> split(std::string_view strv,
+                                    std::string_view delims = " ",
+                                    split_options const options = split_options::exclude_delimiters);
 
 } // utils
 
